f79b160fd461bf4f949158688a85728e6f6a06e7
Hamza <hamzatarar8@gmail.com>
2021-12-24 12:11:08 +0200

    Made recommended changes.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({
@@ -8,6 +9,7 @@ const reducer = combineReducers({
 
 const store = createStore(
   reducer,
+  applyMiddleware(logger)
 );
 
 export default store;
